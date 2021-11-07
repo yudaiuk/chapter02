1bde7cc0fdbd9ffbeea03d16400742b6c1d21651
yudai <yuudaiufo@icloud.com>
2021-11-07 19:31:24 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('#typo')
+    .on('mouseover', function() {
+      $('#typo').stop(true).animate({
+        backgroundColor: '#ae5e9b'
+      }, 500);
+    })
+    .on('mouseout', function() {
+      $('#typo').stop(true).animate({
+        backgroundColor: '#3498db'
+      }, 500);
+    });
+});
+
+// 1時間
